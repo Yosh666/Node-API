d55412f3d56940d665aa8138cec057dd08848c3a
Yosh666 <audrey1cauvin@gmail.com>
2020-06-24 16:12:41 +0200

    projet fini sauf bug recherche

--- routes/APIHandler.js
+++ routes/APIHandler.js
@@ -2,17 +2,74 @@ var fetch = require('node-fetch')
 
 const key='1b5c74dbc286cbaa892f1a459f8e5e36'
 const lang= 'fr-FR'
-
+const url='https://api.themoviedb.org/3/movie'
 class ApiHandler{
 
-    getMovie(id) {
+     async getMovie(id) {
         
-        return fetch('https://api.themoviedb.org/3/movie/'+id+'?api_key='+key+'&language='+lang)
-        .then(response => response.json())
-        .then(json => json)
-        .catch(error => "Erreur : " + error)
+        let movie = await fetch(url+'/'+id+'?api_key='+key+'&language='+lang)
         
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                return json
+            }).catch(function(ex) {
+               return "error"
+            });
+        let casting = await fetch(url+'/'+id+'/credits?api_key='+key)
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                return json
+            }).catch(function(ex) {
+            return "error"
+            });
+
+        return {
+            movie: movie,
+            casting:casting
+        }
+    }
+    searchMovie(query){
+        return fetch('https://api.themoviedb.org/3/search/movie?api_key='+key+'&language='+lang+'&query='+query+'&page=1&include_adult=false')
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                return json
+            }).catch(function(ex) {
+            return "error"
+            });
+        
+
+    }
+     async getActor(id){
+        let actor =await fetch('https://api.themoviedb.org/3/person/'+id+'?api_key='+key+'&language='+lang)
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                return json
+            }).catch(function(ex) {
+            return "error"
+            });
+        let filmo =await fetch('https://api.themoviedb.org/3/person/'+id+'/movie_credits?api_key='+key+'&language='+lang)
+            .then(function(response) {
+                return response.json()
+            }).then(function(json) {
+                return json
+            }).catch(function(ex) {
+            return "error"
+            });
+        return{
+            actor:actor,
+            filmo:filmo
+        }
+
     }
 }
 
 module.exports = new ApiHandler()
+/*choper le cast:https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>*/
+/*https://api.themoviedb.org/3/movie/550/credits?api_key=1b5c74dbc286cbaa892f1a459f8e5e36&language=fr-FR*/
+/*query les movies: https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=vroumm&page=1&include_adult=false*/
+/*choper l'acteur: https://api.themoviedb.org/3/person/{person_id}?api_key=<<api_key>>&language=en-US*/
+/* choper la filmo:https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=<<api_key>>&language=en-US*/
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,53 @@ var express = require('express');
 var router = express.Router();
 var api = require('./APIHandler')
 
-/* GET home page. */
-router.get('/:id?',  async function(req, res, next) {
 
-  let id =req.params.id
-  if(id === undefined){
-    id=550
-  }
-  let movie= await api.getMovie(id)
+/*GET homepage */
+router.get('/',  async function(req, res, next) {
 
-  res.render('index', { 
-    title: 'API + Node', 
-    movie:'movie'
-  });
+  let query = req.query.query
+
+  
+  let movies = await api.searchMovie(query)
+  console.log(movies)
+
+  res.render('index_search', { 
+    title: 'Accueil',
+    movies:movies.results
+    
+   });
+
+});
+
+/* GET viewpage. */
+router.get('/:id',  async function(req, res, next) {
+
+  let id =req.params.id 
+  
+  let movieAndCast= await api.getMovie(id)
+  
+
+  res.render('movie_view', { 
+    title: 'Détails du film', 
+    movie: movieAndCast.movie,
+    casting: movieAndCast.casting
+   });
+});
+
+/*Get actorpage*/
+router.get('/actor/:id',  async function(req, res, next) {
+
+  let id =req.params.id 
+  
+  let actorAndFilmo= await api.getActor(id)
+  
+
+  res.render('actor_view', { 
+    title: 'Détails de l\'acteur', 
+    actor:actorAndFilmo.actor,
+    filmo:actorAndFilmo.filmo
+   });
 });
 
 module.exports = router;
+
